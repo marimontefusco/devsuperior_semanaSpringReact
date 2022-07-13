6f376fe2369baa08424611fad45800d001cabd6b
marimontefusco <mari.montefusco@gmail.com>
2022-07-13 15:21:57 -0300

    first component - notification button

---
+++ frontend/src/assets/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../img/notification-icon.svg';
+import './style.css';
+
+function NotificationButton() {
+    return(
+        <div className="dsmeta-red-btn">
+        <img src={icon} alt="Ícone de notificação" />
+      </div>
+    )
+  }
+  
+  export default NotificationButton
+  
